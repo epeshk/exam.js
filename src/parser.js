5386dc46132f2597b93c4c018116df4bb5cc5dd4
Sergey Golovin <golovim@gmail.com>
2014-06-23 20:55:00 +0400

    function isEmpty(obj) was added

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,14 @@
-function Parser(){
+function Parser() {
     'use strict';
-    if(!(this instanceof Parser)){
-    	return new Parser();
+    if (!(this instanceof Parser)) {
+        return new Parser();
     }
     var self = this;
 
     self._blockPattern = /\{\{(.|\n)*?\}\}/g;
 }
 
-Parser.prototype._parseSyntaxBlocks = function(text){
+Parser.prototype._parseSyntaxBlocks = function(text) {
     var self = this;
     var regexp = new RegExp(self._blockPattern);
     var result = text.match(regexp);
@@ -16,3 +16,30 @@ Parser.prototype._parseSyntaxBlocks = function(text){
     return result;
 };
 
+Parser.prototype._extractObjects = function(syntaxBlocks) {
+    var self = this;
+    var result = [];
+
+    function isEmpty(obj) {
+        if (obj === null) {
+            return true;
+        }
+        if (obj.length > 0) {
+            return false;
+        }
+        if (obj.length === 0) {
+            return true;
+        }
+        for (var key in obj) {
+            if (hasOwnProperty.call(obj, key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    syntaxBlocks.forEach(function(block) {
+
+    });
+};
