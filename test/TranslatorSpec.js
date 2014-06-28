c959d42b46d07ae8452503ae55b596cc7dcd2ece
Sergey Golovin <golovim@gmail.com>
2014-06-28 16:01:05 +0400

    typo in TranslatorSpec.js was fixed

--- test/TranslatorSpec.js
+++ test/TranslatorSpec.js
@@ -21,7 +21,7 @@ describe('Translator', function() {
         it('should create a listbox from List', function() {
             var result = translator._createListBox(new List(['test1', 'test2'], 1, '{{test1,!test2!}}'));
 
-            expect(result).toBe('<input list="testID"><datalist id="testID"i><option value="test1"><option value="test2"');
+            expect(result).toBe('<input list="testID"><datalist id="testID"i><option value="test1"><option value="test2">');
         });
     });
 });
