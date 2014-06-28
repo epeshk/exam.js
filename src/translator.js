cb46429f8c8ed1db74490be683a8516210492c7e
Sergey Golovin <golovim@gmail.com>
2014-06-28 15:51:54 +0400

    _getNexID function was added

--- src/translator.js
+++ src/translator.js
@@ -4,7 +4,13 @@ function Translator(){
         return new Translator();
     }
     var self = this;
+    self._currentID = 0;
 }
+
+Translator.prototype._getNextID = function(){
+    var self = this;
+    return 'examjs_id_' + (++self._currentID); 
+};
 
 Translator.prototype._createListBox = function(listObject, id){
 
