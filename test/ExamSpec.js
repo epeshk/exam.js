189f5db8f333932ebeb0d634d4f6e01cc9cbee42
Sergey Golovin <golovim@gmail.com>
2014-06-30 12:10:19 +0400

    Test for parse() method was added

--- test/ExamSpec.js
+++ test/ExamSpec.js
@@ -1,19 +1,15 @@
 'use strict';
-describe('ExamJS', function(){
+describe('Exam', function() {
     var exam;
-    beforeEach(function(){
-       exam = new Exam(); 
-    });
-
-    it('should be created', function(){
-        var result = new Exam();
+    beforeEach(function() {
+        exam = new Exam();
     });
 
     describe('parse()', function(){
-        it('should parse source text and replace all syntax blocks to HTML elements', function(){
+        it('should replace all syntax blocks to HTML tags', function(){
             var result = exam.parse('bla bla bla {{test1,!test2!}} bla');
 
             expect(result).toBe('bla bla bla <input list="examjs_id_1"><datalist id="examjs_id_1"><option value="test1"><option value="test2"></datalist> bla');
-        }); 
+        });
     });
 });
