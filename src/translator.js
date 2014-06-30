7dfae21ac840dabb1b3bf18a9754040290dff585
Sergey Golovin <golovim@gmail.com>
2014-06-30 12:16:55 +0400

    _convertAllObjects returns an object with source and result properties now

--- src/translator.js
+++ src/translator.js
@@ -1,40 +1,43 @@
-function Translator(){
+function Translator() {
     'use strict';
-    if(!(this instanceof Translator)){
+    if (!(this instanceof Translator)) {
         return new Translator();
     }
     var self = this;
     self._currentID = 0;
 }
 
-Translator.prototype._getNextID = function(){
+Translator.prototype._getNextID = function() {
     var self = this;
-    return 'examjs_id_' + (++self._currentID); 
+    return 'examjs_id_' + (++self._currentID);
 };
 
-Translator.prototype._createListBox = function(listObject){
+Translator.prototype._createListBox = function(listObject) {
     var self = this;
     var id = self._getNextID();
     var result = '<input list="' + id + '">';
     result += '<datalist id="' + id + '">';
 
-    listObject.items.forEach(function(item){
+    listObject.items.forEach(function(item) {
         result += '<option value="' + item + '">';
-    }); 
+    });
     result += '</datalist>';
 
     return result;
 };
 
-Translator.prototype._convertAllObjects = function(objects){
+Translator.prototype._convertAllObjects = function(objects) {
     var self = this;
     var result = [];
-    objects.forEach(function(object){
-        if(object instanceof List){
-            result.push(self._createListBox(object));
-        } else{
+    objects.forEach(function(object) {
+        if (object instanceof List) {
+            result.push({
+                source: object.syntaxBlock,
+                result: self._createListBox(object)
+            });
+        } else {
             throw new Error('Converting error. Translator cannot convert object that was passed into it');
-        }   
+        }
     });
 
     return result;
