854732b0e87ea25352c72bbfbae1b7a9dc032143
Sergey Golovin <golovim@gmail.com>
2014-07-25 12:56:37 +0400

    Lexems classes were added

--- build/exam.js
+++ build/exam.js
@@ -1724,7 +1724,7 @@ function merge_text_nodes( jsonml ) {
   }
 } )() );
 
-function Lexer(){
+function Lexer() {
     'use strict';
     var self = this;
     self.ANSWER_SPTR = 0;
@@ -1733,17 +1733,42 @@ function Lexer(){
 
     self.tokens = {
         '::': self.ANSWER_SPTR,
-        ',' : self.ITEMS_SPTR,
-        ':?' : self.HELP_SPTR
+        ',': self.ITEMS_SPTR,
+        ':?': self.HELP_SPTR
     };
 }
 
+function extend(Child, Parent) {
+    var F = function() {};
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+    Child.superclass = Parent.prototype;
+}
 
-Lexer.prototype._range = function(n){
+function Lexem(value) {
+    'use strict';
+    var self = this;
+    self.value = value;
+}
+
+function Item(value) {}
+function AnswerSeparator(value) {}
+function ItemsSeparator(value){}
+function HelpSeparator(value){}
+
+extend(Item,Lexem);
+extend(AnswerSeparator, Lexem);
+extend(ItemsSeparator, Lexem);
+extend(HelpSeparator, Lexem);
+
+Lexer.prototype._range = function(n) {
     'use strict';
     var self = this;
 
-    return Array.apply(null,new Array(n)).map(function (_, i) {return i;});
+    return Array.apply(null, new Array(n)).map(function(_, i) {
+        return i;
+    });
 };
 
 function ParsingError(message) {
--- src/lexer.js
+++ src/lexer.js
@@ -1,4 +1,4 @@
-function Lexer(){
+function Lexer() {
     'use strict';
     var self = this;
     self.ANSWER_SPTR = 0;
@@ -7,15 +7,40 @@ function Lexer(){
 
     self.tokens = {
         '::': self.ANSWER_SPTR,
-        ',' : self.ITEMS_SPTR,
-        ':?' : self.HELP_SPTR
+        ',': self.ITEMS_SPTR,
+        ':?': self.HELP_SPTR
     };
 }
 
+function extend(Child, Parent) {
+    var F = function() {};
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+    Child.superclass = Parent.prototype;
+}
 
-Lexer.prototype._range = function(n){
+function Lexem(value) {
+    'use strict';
+    var self = this;
+    self.value = value;
+}
+
+function Item(value) {}
+function AnswerSeparator(value) {}
+function ItemsSeparator(value){}
+function HelpSeparator(value){}
+
+extend(Item,Lexem);
+extend(AnswerSeparator, Lexem);
+extend(ItemsSeparator, Lexem);
+extend(HelpSeparator, Lexem);
+
+Lexer.prototype._range = function(n) {
     'use strict';
     var self = this;
 
-    return Array.apply(null,new Array(n)).map(function (_, i) {return i;});
+    return Array.apply(null, new Array(n)).map(function(_, i) {
+        return i;
+    });
 };
