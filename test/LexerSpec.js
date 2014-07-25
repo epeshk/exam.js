58cc6870e301ec0ca18e04fa9eea0c8f341a01f1
Sergey Golovin <golovim@gmail.com>
2014-07-25 12:32:15 +0400

    one more test for _range() method

--- test/LexerSpec.js
+++ test/LexerSpec.js
@@ -9,6 +9,13 @@ describe('Lexer',function(){
         it('should return a range of a numbers starting with zero and ending with N - 1', function(){
             var result = lexer._range(10);
 
+            expect(result[0]).toEqual(0);
+            expect(result[9]).toEqual(9);
+        });
+
+        it('should return a range of a numbers with length equal N - 1',function(){
+            var result = lexer._range(10);
+
             expect(result.length).toEqual(10);
         });
     });
