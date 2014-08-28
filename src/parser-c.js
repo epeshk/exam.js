e1d81069c3c070ad334825a0db5e4c458570d120
Sergey Golovin <golovim@gmail.com>
2014-08-28 21:17:28 +0400

    tmp parser-c.js was added

---
+++ src/parser-c.js
@@ -0,0 +1,58 @@
+(function() {
+  var CheckBox, ExamObject, List, TextInput,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  ExamObject = (function() {
+    function ExamObject(syntaxBlock, helpText, id) {
+      this.syntaxBlock = syntaxBlock;
+      this.helpText = helpText;
+      this.id = id;
+      if (helpText) {
+        this._helpTagId = "help_" + this.id;
+      }
+    }
+
+    return ExamObject;
+
+  })();
+
+  List = (function(_super) {
+    __extends(List, _super);
+
+    function List(syntaxBlock, helpText, id, items, rightAnswerIndex) {
+      List.__super__.constructor.call(this, syntaxBlock, helpText, id);
+      this.items = items;
+      this.rightAnswerIndex = rightAnswerIndex;
+    }
+
+    return List;
+
+  })(ExamObject);
+
+  TextInput = (function(_super) {
+    __extends(TextInput, _super);
+
+    function TextInput(syntaxBlock, helpText, id, rightAnswer) {
+      TextInput.__super__.constructor.call(this, syntaxBlock, helpText, id);
+      this.rightAnswer = rightAnswer;
+    }
+
+    return TextInput;
+
+  })(ExamObject);
+
+  CheckBox = (function(_super) {
+    __extends(CheckBox, _super);
+
+    function CheckBox(syntaxBlock, helpText, id, items, rightAnswerIndex) {
+      CheckBox.__super__.constructor.call(this, syntaxBlock, helpText, id);
+      this.items = items;
+      this.rightAnswerIndex = rightAnswerIndex;
+    }
+
+    return CheckBox;
+
+  })(ExamObject);
+
+}).call(this);
