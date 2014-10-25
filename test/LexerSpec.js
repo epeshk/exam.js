35e706c3676e929b118256d0486c47399be7f282
Sergey Golovin <golovim@gmail.com>
2014-10-25 15:29:54 +0400

    tests for all tokens have been added

--- test/LexerSpec.js
+++ test/LexerSpec.js
@@ -12,7 +12,7 @@ describe('Lexer', function() {
             expect(result.expression.length).toEqual(5);
         });
 
-        it('should return an expression that contains StartBLock, two Items, one ItemsSeparator and EndBlock', function(){
+        it('should return an expression that contains StartBLock, two Items, one ItemsSeparator and EndBlock', function() {
             var result = lexer.parse('{{1,2}}');
 
             expect(result.expression[0] instanceof StartBlock).toBeTruthy();
@@ -22,9 +22,9 @@ describe('Lexer', function() {
             expect(result.expression[4] instanceof EndBlock).toBeTruthy();
         });
 
-        it('should return an expression that contains all types of a tokens', function(){
+        it('should return an expression that contains all types of a tokens', function() {
             var result = lexer.parse('{{1,2,... :: test text :? help text}}');
-            
+
             expect(result.expression.length).toEqual(11);
             expect(result.expression[0] instanceof StartBlock).toBeTruthy();
             expect(result.expression[1] instanceof Item).toBeTruthy();
@@ -39,7 +39,7 @@ describe('Lexer', function() {
             expect(result.expression[10] instanceof EndBlock).toBeTruthy();
         });
 
-        it('should return an expression that contains StartSection, Item and EndSection', function(){
+        it('should return an expression that contains StartSection, Item and EndSection', function() {
             var result = lexer.parse('{-- test --}');
             expect(result.expression.length).toEqual(3);
 
@@ -48,7 +48,7 @@ describe('Lexer', function() {
             expect(result.expression[2] instanceof EndSection).toBeTruthy();
         });
 
-        it('should return an expression that contains StartSection, StartBlock, Item, EndBlock, EndSection', function(){
+        it('should return an expression that contains StartSection, StartBlock, Item, EndBlock, EndSection', function() {
             var result = lexer.parse('{-- {{test}} --}');
             expect(result.expression.length).toEqual(5);
 
@@ -59,55 +59,91 @@ describe('Lexer', function() {
             expect(result.expression[4] instanceof EndSection).toBeTruthy();
         });
 
-        it('should parse all tokens from a source', function(){
+        it('should parse all tokens from a source', function() {
             var result = lexer.parse('{--\n Some text, some text, some text {{1,2::1:?test}} --}');
 
             expect(result.expression.length).toEqual(17);
         });
 
-        it('should parse "\\n" as token', function(){
+        it('should parse "\\n" as token', function() {
             var result = lexer.parse('\n');
 
             expect(result.expression[0] instanceof EndOfLine).toBeTruthy();
         });
     });
 
-    describe('tryToAddSeparator()', function(){
-        it('should return an ItemsSeparator', function(){
+    describe('tryToAddSeparator()', function() {
+        it('should return an ItemsSeparator', function() {
             var result = lexer._tryToAddSeparator([], lexer.tokens.ITEMS_SPTR);
 
             expect(result[0] instanceof ItemsSeparator);
         });
 
-        it('should return an AnswerSeparator', function(){
+        it('should return an AnswerSeparator', function() {
             var result = lexer._tryToAddSeparator([], lexer.tokens.ANSWER_SPTR);
 
             expect(result[0] instanceof AnswerSeparator);
         });
 
-        it('should return a HelpSeparator', function(){
+        it('should return a HelpSeparator', function() {
             var result = lexer._tryToAddSeparator([], lexer.tokens.HELP_SPTR);
 
             expect(result[0] instanceof HelpSeparator);
         });
+
+        it('should return a InputToken', function() {
+            var result = lexer._tryToAddSeparator([], lexer.tokens.INPUT_TOKEN);
+
+            expect(result[0] instanceof InputToken);
+        });
+
+        it('should return a StartBlock', function() {
+            var result = lexer._tryToAddSeparator([], lexer.tokens.START_BLOCK_TOKEN);
+
+            expect(result[0] instanceof StartBlock);
+        });
+
+        it('should return a EndBlock', function() {
+            var result = lexer._tryToAddSeparator([], lexer.tokens.END_BLOCK_TOKEN);
+
+            expect(result[0] instanceof EndBlock);
+        });
+
+        it('should return a StartSection', function() {
+            var result = lexer._tryToAddSeparator([], lexer.tokens.START_SECTION_TOKEN);
+
+            expect(result[0] instanceof StartSection);
+        });
+
+        it('should return a EndSection', function() {
+            var result = lexer._tryToAddSeparator([], lexer.tokens.END_SECTION_TOKEN);
+
+            expect(result[0] instanceof EndSection);
+        });
+
+        it('should return a EndOfLine', function() {
+            var result = lexer._tryToAddSeparator([], lexer.tokens.END_OF_LINE);
+
+            expect(result[0] instanceof EndOfLine);
+        });
     });
 
-    describe('_isEmpty()', function(){
-        it('should return true if string is empty', function(){
+    describe('_isEmpty()', function() {
+        it('should return true if string is empty', function() {
             var result = lexer._isEmpty('    ');
-            
-            expect(result).toBeTruthy();
-        });  
 
-        it('should return false if string is not empty', function(){
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if string is not empty', function() {
             var result = lexer._isEmpty('  test ');
-            
+
             expect(result).toBeFalsy();
-        });  
+        });
     });
 
-    describe('_isPartOfToken()', function(){
-        it('shoud return True if the tokens contains a char', function(){
+    describe('_isPartOfToken()', function() {
+        it('shoud return True if the tokens contains a char', function() {
             var result1 = lexer._isPartOfToken(',');
             var result2 = lexer._isPartOfToken(':');
             var result3 = lexer._isPartOfToken('.');
@@ -117,9 +153,9 @@ describe('Lexer', function() {
             expect(result2).toBeTruthy();
             expect(result3).toBeTruthy();
             expect(result4).toBeTruthy();
-        });  
+        });
 
-        it('should return True if the tokens contains a string', function(){
+        it('should return True if the tokens contains a string', function() {
             var result1 = lexer._isPartOfToken('::');
             var result2 = lexer._isPartOfToken(':?');
             var result3 = lexer._isPartOfToken('..');
@@ -129,7 +165,7 @@ describe('Lexer', function() {
             expect(result3).toBeTruthy();
         });
 
-        it('should return False if the tokens not contains a string', function(){
+        it('should return False if the tokens not contains a string', function() {
             var result1 = lexer._isPartOfToken(':.');
             var result2 = lexer._isPartOfToken(':,');
             var result3 = lexer._isPartOfToken('.,');
@@ -140,20 +176,20 @@ describe('Lexer', function() {
         });
     });
 
-    describe('_isToken()', function(){
-        it('should return True if string that was passed into this method is a syntax token', function(){
+    describe('_isToken()', function() {
+        it('should return True if string that was passed into this method is a syntax token', function() {
             var result1 = lexer._isToken('...');
             var result2 = lexer._isToken('::');
             var result3 = lexer._isToken(':?');
             var result4 = lexer._isToken(',');
-            
+
             expect(result1).toBeTruthy();
             expect(result2).toBeTruthy();
             expect(result3).toBeTruthy();
             expect(result4).toBeTruthy();
-        });  
+        });
 
-        it('should return False if string that was passed into this method is not a syntax token', function(){
+        it('should return False if string that was passed into this method is not a syntax token', function() {
             var result1 = lexer._isToken('..,');
             var result2 = lexer._isToken('');
 
