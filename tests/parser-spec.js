e25e11c64b702a26a7e77c103f2906054086deed
Sergey Golovin <golovim@gmail.com>
2015-03-13 21:50:09 +0300

    Test for checking count of expressions was added

--- tests/parser-spec.js
+++ tests/parser-spec.js
@@ -2,10 +2,15 @@ var assert = require('assert'),
     exam = require('../src/exam.js');
 
 describe('Parser tests', function(){
-  describe('parse()', function(){
+  describe('Simple expressions parser', function(){
     it('should return an object with a expressions', function(){
       var result = exam.parse('{{ test? :? ... :: test }}');
       assert.equal(result.expressions != null, true);
-    })
-  })
+    });
+
+    it('should return an object with a expressions field than contains proper count of expressions', function(){
+      var result = exam.parse('{{ test? :? ... :: test }} {{ test? :? ... :: test }}');
+      assert.equal(result.expressions.length, 2);
+    });
+  });
 })
