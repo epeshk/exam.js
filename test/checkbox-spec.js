8d9026cbec30478a68be9b5c2bc5a89df8804eb2
Sergey Golovin <golovim@gmail.com>
2015-03-19 23:31:47 +0300

    tested output html

--- test/checkbox-spec.js
+++ test/checkbox-spec.js
@@ -6,8 +6,8 @@ describe('Checkbox tests', function() {
     it('should return object which contains expressions with type "checkbox"', function() {
       var result = exam.parse('{{ question1? :? 1,2,3 :: 1,3 }} {{ question2? :? 1,2,3 :: 1,3 }}');
       console.log(result);
-      assert.equal(result.expressions[0].type, 'checkbox');
-      assert.equal(result.expressions[1].type, 'checkbox');
+      assert.equal(result.expressions[0].html, '<div id="exam-js-0"> question1? <input id="exam-js-1" type="checkbox" class="exam-js-input"> 1</input>,<input id="exam-js-2" type="checkbox" class="exam-js-input">2</input>,<input id="exam-js-3" type="checkbox" class="exam-js-input">3 </input></div>');
+      assert.equal(result.expressions[1].html, '<div id="exam-js-4"> question2? <input id="exam-js-5" type="checkbox" class="exam-js-input"> 1</input>,<input id="exam-js-6" type="checkbox" class="exam-js-input">2</input>,<input id="exam-js-7" type="checkbox" class="exam-js-input">3 </input></div>');
     });
   });
 })
