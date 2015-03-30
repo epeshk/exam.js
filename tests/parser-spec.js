de103bb813f76773048514eeefce5d4f3ecb1209
htyrmd <hitymind@gmail.com>
2015-03-30 13:23:27 +0300

    Added list test

--- tests/parser-spec.js
+++ tests/parser-spec.js
@@ -36,13 +36,13 @@ describe('Parser tests', function(){
       assert.equal(result.expressions[1].id.indexOf('exam-js-') >= 0, true);
     });
 
-    // it('should parse list expressions', function(){
-    //   var result = exam.parse('{{ question1? :? 1,2,3 :: 2 }}');
-    //   assert.equal(result.expressions[0] != null, true);
-    // });
+    it('should parse list expressions', function(){
+      var result = exam.parse('{{ question1? :? 1,2,3 :: 2 }}');
+      assert.equal(result.expressions != null, true);
+    });
 
-    // it('should return object which contains expressions with type "list"',function(){
-    //   var result = exam.parse('{{ question1? :? 1,2,3 :: 2 }} {{ question2? :? 1,2,3:: 3 }}');
+    // it('should return object which contains expressions with type "list"', function(){
+    //   var result = exam.parse('{{ question1? :? 1,2,3 :: 1 }} {{ question2? :? 1,2,3:: 3 }}');
     //   assert.equal(result.expressions[0].type, 'list');
     //   assert.equal(result.expressions[1].type, 'list');
     // });
