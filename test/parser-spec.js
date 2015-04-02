7f0aaf2c69fea0404ade887c8413dd69d6bf0304
Sergey Golovin <golovim@gmail.com>
2015-04-02 21:30:39 +0300

    Check generation of a not expression's html

--- test/parser-spec.js
+++ test/parser-spec.js
@@ -17,5 +17,12 @@ describe('Parser tests', function(){
       var result = exam.parse('\nТЕСТ\nWhat is your name?\n+Exam.js\n\n');
       assert.equal(result.expressions[0].html.length > 0, true);
     });
+
+    it('should return an object with an input html which contains some text except expressions', function(){
+      var result = exam.parse('bla bla bla\nТЕСТ\nWhat is your name?\n+Exam.js\n\nblah blah blah');
+      console.log(result.html);
+      assert.equal(result.html.indexOf('bla bla bla') > 0, true);
+      assert.equal(result.html.indexOf('blah blah blah') > 0, true);
+    });
   });
 })
