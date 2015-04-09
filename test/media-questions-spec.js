b1281c9c3cc7406587f5088ba2cb6594413b3fa1
Sergey Golovin <golovim@gmail.com>
2015-04-09 04:54:26 +0300

    test for media question was added (audio)

--- test/media-questions-spec.js
+++ test/media-questions-spec.js
@@ -7,5 +7,10 @@ describe('Media question tests', function() {
       var result = exam.parse('ТЕСТ ВИДЕО\rWhat is your name?\r+Exam.js\rTest\r\r');
       assert.equal(result.expressions[0].type === 'VIDEO-QUESTION', true);
     });
+
+    it('should return proper type of expression', function() {
+      var result = exam.parse('ТЕСТ АУДИО\rWhat is your name?\r+Exam.js\rTest\r\r');
+      assert.equal(result.expressions[0].type === 'AUDIO-QUESTION', true);
+    });
   });
 })
