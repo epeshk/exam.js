843a4e87a4bdcaeddb10d7e3eca77f1ec73e95a8
Sergey Golovin <golovim@gmail.com>
2015-04-24 04:57:00 +0300

    Check count for right answers

--- test/question-spec.js
+++ test/question-spec.js
@@ -7,5 +7,13 @@ describe('Question tests', function(){
       var result = exam.parse('ТЕСТ\rWhat is your name?\r+Exam.js\rTest\r\r');
       assert.equal(result.expressions[0].answers.length === 2, true);
     });
+
+    it('should return an object with 2 right answers', function(){
+      var result = exam.parse('ТЕСТ\rWhat is your name?\r+Exam.js\r+Test1\rTest2\r\r');
+      var count = result.expressions[0].answers.filter(function(a){
+        return a.isRight;
+      }).length;
+      assert.equal(count === 2, true);
+    });
   });
 })
