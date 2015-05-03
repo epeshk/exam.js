e93cf0ce0a032ba36c12af47267176073114b907
Sergey Golovin <golovim@gmail.com>
2015-05-03 04:50:25 +0300

    useless code was deleted

--- test/media-questions-spec.js
+++ test/media-questions-spec.js
@@ -5,7 +5,6 @@ describe('Media question tests', function() {
   describe('VIDEO question', function() {
     it('should return proper type of expression', function() {
       var result = exam.parse('ТЕСТ ВИДЕО\rWhat is your name?\r+Exam.js\rTest\r\r');
-      console.log(result.expressions[0].type);
       assert.equal(result.expressions[0].type === 'VIDEO-QUESTION', true);
     });
   });
