73d242a0243f500eceedca3084e9b2d50f5907ba
Sergey Golovin <golovim@gmail.com>
2015-05-04 12:08:23 +0300

    Description of a media test's media section was changed

--- test/media-questions-spec.js
+++ test/media-questions-spec.js
@@ -2,7 +2,7 @@ var assert = require('assert'),
   exam = require('../src/exam.js');
 
 describe('Media question tests', function() {
-  describe('VIDEO question', function() {
+  describe('common media type question', function() {
     it('should return proper type of expression', function() {
       var result = exam.parse('ТЕСТ ВИДЕО\rWhat is your name?\r+Exam.js\rTest\r\r');
       assert.equal(result.expressions[0].type === 'VIDEO-QUESTION', true);
