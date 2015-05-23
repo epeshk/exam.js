d580969d28326092858c027d2e340a0361f5f24d
Sergey Golovin <golovim@gmail.com>
2015-05-23 15:28:59 +0300

    add simple test for question object

--- test/question-spec.js
+++ test/question-spec.js
@@ -2,7 +2,10 @@ var assert = require('assert'),
     exam = require('../dist/exam.js');
 
 describe('Question tests', function(){
-  describe('Simple question tests', function(){
-
+  describe('question object', function(){
+    it('should contain right answer', function(){
+      var result = exam.parse('ТЕСТЫ\r\rТЕКСТ\r\rWhat is your name?\r+Exam.js\r\rКОНЕЦ ТЕСТОВ');
+      assert.equal(result.expressions[0].questions[0].answers[0].answer === 'Exam.js', true);
+    });
   });
 })
