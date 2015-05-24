17f642203891d375247f472b6c49981a2948b818
Sergey Golovin <golovim@gmail.com>
2015-05-24 15:33:53 +0300

    add simple test for question object

--- test/question-spec.js
+++ test/question-spec.js
@@ -10,8 +10,8 @@ describe('Question tests', function(){
 
     it('should contain "on answer" callback', function(){
       var result = exam.parse('ТЕСТЫ\r\rТЕКСТ\r\rWhat is your name?\r+Exam.js\r\rКОНЕЦ ТЕСТОВ');
-      var cb = result.expressions[0].questions[0].answers[0].onAnswer;
-      assert.equal(cb != null && typeof cb === 'Function', true);
+      var cb = result.expressions[0].questions[0].onAnswer;
+      assert.equal(cb != null && typeof cb === 'function', true);
     });
   });
 })
