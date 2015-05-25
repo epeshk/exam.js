92f0599812bf275eecfa3460faba755c55f4461f
Sergey Golovin <golovim@gmail.com>
2015-05-25 15:40:50 +0300

    one more test

--- test/question-spec.js
+++ test/question-spec.js
@@ -19,5 +19,12 @@ describe('Question tests', function(){
       var html = result.expressions[0].questions[0].html;
       assert.equal(html != null && typeof html === 'string', true);
     });
+
+    it('should contain html markup of a question', function(){
+      var result = exam.parse('ТЕСТЫ\r\rТЕКСТ\r\rWhat is your name?\r+Exam.js\r\rКОНЕЦ ТЕСТОВ');
+      console.log(result.expressions[0].questions[0]);
+      var id = result.expressions[0].questions[0].htmlID;
+      assert.equal(id != null && typeof id === 'string', true);
+    });
   });
 })
