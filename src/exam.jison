278a8dac0ef86f84ed85620c211c50048e189395
Sergey Golovin <golovim@gmail.com>
2015-09-19 00:00:23 +0300

    init demo site

---
+++ src/exam.jison
@@ -0,0 +1,191 @@
+%{
+  _examjsParser = new ExamJS();
+%}
+/* lexical grammar */
+%lex
+%%
+
+"ТЕСТЫ"                            return 'TESTS'  //start tests block
+"КОНЕЦ ТЕСТОВ"                     return 'TESTS_END'
+"ТЕКСТ"                            return 'TEXT'  //start text block
+"ВИДЕО"                            return 'VIDEO' //type "video" marker
+"АУДИО"                            return 'AUDIO' //type "audio" marker
+"РИСУНОК"                          return 'IMAGE' //type "image" marker
+(\n|\r|\r\n)                       return 'SEP'  //separator
+\s                                 return 'SP'
+^"+"                               return '+' //right answer marker
+^"-"                               return '-' //wrong answer marker
+[^(\s|\n|\r|\n\r)]                 return 'char'
+[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/] return 'special_symbol'
+<<EOF>>                            return 'EOF'
+/lex
+
+%start file
+
+%% /* language grammar */
+
+symbol
+  : 'SP'
+    {$$ = $1}
+  | 'char'
+    {$$ = $1}
+  | 'SEP'
+    {$$ = $1}
+  | 'special_symbol'
+    {$$ = $1}
+  | '+'
+    {$$ = $1}
+  | '-'
+    {$$ = $1}
+  ;
+
+phrase
+  : symbol
+    {$$ = '' + $1}
+  | phrase symbol
+    {$$ = $1 + $2}
+  ;
+
+empty_lines
+  : 'SEP'
+  | empty_lines 'SEP'
+  ;
+
+AM
+  : '+'
+    {$$ = true}
+  | '-'
+    {$$ = false}
+  ;
+
+answer
+  : AM phrase 'SEP'
+    {$$ = {answer: $2, isRight: $1}}
+  ;
+
+answers
+  : answer
+    {$$ = {answers: [$1]}}
+  | answers answer
+    {$$.answers.push($2)}
+  ;
+
+question
+  : empty_lines phrase 'SEP' answers
+    {$$ = {question: $2, answers: $4.answers}}
+  ;
+
+type
+  : 'TEXT'
+    {$$ = 'TEXT'}
+  | 'VIDEO'
+    {$$ = 'VIDEO'}
+  | 'AUDIO'
+    {$$ = 'AUDIO'}
+  | 'IMAGE'
+    {$$ = 'IMAGE'}
+  ;
+
+type_marker
+  : empty_lines type
+    {$$ = $2}
+  ;
+
+test_block
+  : question
+    {$$ = _examjsParser.createQuestions($1)}
+  | type_marker
+    {_examjsParser.setCurrentType($1)}
+  ;
+
+test_blocks
+  : test_block
+    {
+      if($1.html){
+        $$ = {questions: [$1]};
+      } else {
+        $$ = {questions: []};
+      }
+    }
+  | test_blocks test_block
+    {
+      if($2.html){
+        $$.questions.push($2);
+      }
+    }
+  ;
+
+tests_section
+  : 'TESTS' empty_lines test_blocks empty_lines 'TESTS_END'
+    {$$ = {questions: $3.questions, type: 'tests-section'}}
+  | 'TESTS' test_blocks empty_lines 'TESTS_END'
+    {$$ = {questions: $2.questions, type: 'tests-section'}}
+  | 'TESTS' empty_lines 'TESTS_END'
+    {$$ = {questions: [], type: 'tests-section'}}
+  | 'TESTS'
+    {$$ = {questions: [], type: 'tests-section'}}
+  | 'TESTS_END'
+    {$$ = {questions: [], type: 'tests-section'}}
+  | 'TESTS' empty_lines
+    {$$ = {questions: [], type: 'tests-section'}}
+  | 'TESTS_END' empty_lines
+    {$$ = {questions: [], type: 'tests-section'}}
+  ;
+
+statement
+  : tests_section
+    {$$ = $1}
+  | phrase
+    {$$ = _examjsParser.parseMarkdown($1)}
+  ;
+
+source
+  : statement
+    {
+      if($1.type === 'tests-section'){
+        var tmpHtml = '',
+            questionsCount = 0;
+        $1.questions.forEach(function(q){
+          if(q.html){
+            questionsCount++;
+            return tmpHtml += q.html;
+          }
+        });
+        $$ = {
+          expressions: [$1],
+          questionsCount: questionsCount,
+          html: tmpHtml
+        }
+      } else {
+        $$ = {
+          expressions: [],
+          questionsCount: 0,
+          html: '<div>' + $1 + '</div>'
+        }
+      }
+    }
+  | source statement
+    {
+      if($2.type === 'tests-section'){
+        var questionsCount = 0;
+        $2.questions.forEach(function(q){
+          if(q.html){
+            questionsCount++;
+            $1.html += q.html;
+          }
+        });
+        $1.expressions.push($2);
+        $1.questionsCount = $1.questionsCount + questionsCount;
+      } else {
+        $1.html += '<div>' + $2 + '</div>';
+      }
+      $$ = $1;
+    }
+  ;
+
+file
+  : source 'EOF'
+    {
+      return new QuestionManager($1);
+    }
+  ;
---
+++ src/exam.js
@@ -0,0 +1,180 @@
+'use strict';
+(function() {
+  try {
+    MathJax.Hub.Config({
+      asciimath2jax: {
+        delimiters: [
+          ['{{', '}}']
+        ]
+      }
+    });
+  } catch (e) {}
+  //mock of a markdonw parser (for testing)
+  try {
+    var tmp = markdown.test;
+  } catch (e) {
+    markdown = {
+      toHTML: function(text) {
+        return text;
+      }
+    };
+  }
+
+  function ExamJS() {
+    this.currentId = 0;
+    this.currentGroudId = 0;
+    this.currentMathId = 0;
+    this.currentType = '';
+  }
+
+  ExamJS.prototype.getID = function() {
+    return 'exam-js-' + this.currentId++;
+  };
+
+  ExamJS.prototype.getGroudID = function() {
+    return 'exam-js-group-' + this.currentGroudId++;
+  };
+  ExamJS.prototype.getMathID = function() {
+    return 'exam-js-math-' + this.currentMathId++;
+  };
+  ExamJS.prototype.setCurrentType = function(type) {
+    this.currentType = type;
+  };
+  ExamJS.prototype.createQuestions = function(question) {
+    question.htmlID = this.getID();
+    question.onAnswer = function(e) {
+      this.checkAnswer(e);
+    };
+    if (this.currentType === 'TEXT') {
+      question.html = this.createTextQuestion(question);
+      return question;
+    } else if (this.currentType === 'VIDEO') {
+      question.html = this.createVideoQuestion(question);
+      return question;
+    } else if (this.currentType === 'AUDIO') {
+      question.html = this.createAudioQuestion(question);
+      return question;
+    } else if (this.currentType === 'IMAGE') {
+      question.html = this.createImageQuestion(question);
+      return question;
+    } else {
+      throw new Error('Wrong section type!');
+    }
+  };
+  ExamJS.prototype.createYoutubeHtml = function(answer){
+    var idx = answer.lastIndexOf('/');
+    var youtubeId = answer.substring(idx);
+    return '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + youtubeId + '" frameborder="0" allowfullscreen></iframe>';
+  };
+  ExamJS.prototype.getBasePartOfAnswer = function(answer, type, groupID, answerNumber, dataType, answerClass, answerHTML) {
+    var tmpId = this.getID();
+    return '<div class="' + answerClass + '"><div class="exam-js-answer-container"><div class="exam-js-answer-number"><input id="' + tmpId + '" type="' + type + '" name="' + groupID + '" class="exam-js-input" data-answer="' + answer.answer + '" data-answer-type="' + dataType + '"/> ' + answerNumber + ')' + '</div>' + '<div class="exam-js-answer">' + answerHTML + '</div></div></div>';
+  };
+  ExamJS.prototype.createImgAnswer = function(answer, type, groupID, answerNumber) {
+    var html = '<div><img src="' + answer.answer + '" class="exam-js-img"/></div>';
+    return this.getBasePartOfAnswer(answer, type, groupID, answerNumber, 'image', 'exam-js-img-question', html);
+  };
+  ExamJS.prototype.createAudioAnswer = function(answer, type, groupID, answerNumber) {
+    var html = '<div><audio controls src="' + answer.answer + '" preload="none"/></div>';
+    return this.getBasePartOfAnswer(answer, type, groupID, answerNumber, 'audio', 'exam-js-media-question', html);
+  };
+  ExamJS.prototype.createVideoAnswer = function(answer, type, groupID, answerNumber) {
+    var html = '<div><video controls width="400" height="300" src="' + answer.answer + '" preload="none" class="exam-js-video-answer"/></div>';
+    if(answer.answer.indexOf('youtu') >= 0){
+      html = this.createYoutubeHtml(answer.answer);
+    }
+    return this.getBasePartOfAnswer(answer, type, groupID, answerNumber, 'video', 'exam-js-media-question', html);
+  };
+  ExamJS.prototype.createTextAnswer = function(answer, type, groupID, answerNumber) {
+    var html = '<div>' + answer.answer + '</div>';
+    return this.getBasePartOfAnswer(answer, type, groupID, answerNumber, 'text', 'exam-js-text-question', html);
+  };
+  ExamJS.prototype.createMediaTypedQuestion = function(question, type, answerGenerator) {
+    var groupID = this.getGroudID();
+    var self = this;
+    return '<form id="' + question.htmlID + '" class="exam-js-question">' + '<div>' + question.question + '</div><div>' + question.answers.map(function(a) {
+      return answerGenerator.call(self, a, type, groupID, (question.answers.indexOf(a) + 1));
+    }).reduce(function(a, b) {
+      return a + b;
+    }) + '</div></form>';
+  };
+  ExamJS.prototype.createMediaQuestion = function(question, answerGenerator) {
+    var rightAnswersCount = question.answers.filter(function(a) {
+      return a.isRight;
+    }).length;
+    if (question.answers.length > 1 && rightAnswersCount > 1) {
+      return this.createMediaTypedQuestion(question, 'checkbox', answerGenerator);
+    } else if (question.answers.length > 1 && rightAnswersCount === 1) {
+      return this.createMediaTypedQuestion(question, 'radio', answerGenerator);
+    } else {
+      throw new Error('Unknown media question type!');
+    }
+  };
+  ExamJS.prototype.createVideoQuestion = function(question, answers) {
+    return this.createMediaQuestion(question, this.createVideoAnswer);
+  };
+  ExamJS.prototype.createImageQuestion = function(question) {
+    return this.createMediaQuestion(question, this.createImgAnswer);
+  };
+  ExamJS.prototype.createAudioQuestion = function(question) {
+    return this.createMediaQuestion(question, this.createAudioAnswer);
+  };
+  ExamJS.prototype.createComplexTextQuestion = function(question) {
+    return this.createMediaQuestion(question, this.createTextAnswer);
+  };
+  ExamJS.prototype.createTextQuestion = function(question) {
+    if (question.answers.length === 1) {
+      return this.createInput(question);
+    } else {
+      return this.createComplexTextQuestion(question);
+    }
+  };
+  ExamJS.prototype.createInput = function(question) {
+    return '<form class="exam-js-question">' + question.question + '<input id="' + question.htmlID + '" type="text" class="exam-js-input"/></form>';
+  };
+  ExamJS.prototype.createList = function(question) {
+    var answersHtml = question.answers.map(function(a) {
+      return '<option value="' + a.answer + '">' + a.answer + '</option>\n';
+    }).reduce(function(a, b) {
+      return a + b;
+    });
+    return '<div><form class="exam-js-question">' + question.question + '<select id="' + question.htmlID + '" class="exam-js-input">' + '<option></option>' + answersHtml + '</select></form><div>';
+  };
+  ExamJS.prototype.createCheckbox = function(question) {
+    var answersHtml = question.answers.map(function(a) {
+      return '<div><input type="checkbox" data-answer="' + a.answer + '" class="exam-js-text-checkbox" data-answer-type="text">  ' + a.answer + '</input></div>';
+    }).reduce(function(a, b) {
+      return a + b;
+    });
+
+    return '<form id="' + question.htmlID + '" class="exam-js-question">' + question.question + answersHtml + '</form>';
+  };
+  ExamJS.prototype.parseMarkdown = function(phrase) {
+    var self = this,
+      regexp = /\{\{[^\}\}]*\}\}/gi,
+      matches = {},
+      found;
+    var tmpMatches = phrase.match(regexp);
+    if (tmpMatches && tmpMatches.length && tmpMatches.length > 0) {
+      tmpMatches.forEach(function(match) {
+        var mock = self.getMathID();
+        matches[mock] = match;
+        phrase = phrase.replace(match, mock);
+      });
+      if (window.markdown) {
+        phrase = markdown.toHTML(phrase);
+      }
+      for (var index in matches) {
+        if (matches.hasOwnProperty(index)) {
+          phrase = phrase.replace(new RegExp(index, 'g'), matches[index]);
+        }
+      }
+    } else {
+      return markdown.toHTML(phrase);
+    }
+    return phrase;
+  };
+
+  this.ExamJS = ExamJS;
+})();
+
