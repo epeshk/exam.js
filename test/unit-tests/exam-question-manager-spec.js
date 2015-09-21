121eb418d6ed832906a71f37247ca8c2ef6c5137
Sergey Golovin <golovim@gmail.com>
2015-09-21 20:11:57 +0300

    add tests for QuestionManager

---
+++ test/unit-tests/exam-question-manager-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+describe('QuestionManager', function() {
+  var questionManager;
+  beforeEach(function() {
+    questionManager = new QuestionManager();
+  });
+
+  describe('getResults()', function() {
+    it('should return a result object', function() {
+
+    });
+  });
+});
+
