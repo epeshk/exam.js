5120df12f99fa33466b0e49e6631cac38af69a7c
Sergey Golovin <golovim@gmail.com>
2015-09-21 16:22:54 +0300

    change params in _createAnswerHtml() method

--- dist/exam.js
+++ dist/exam.js
@@ -1922,27 +1922,31 @@ var ExamjsTranslator = (function() {
   };
   ExamjsTranslator.prototype._createAnswerHtml = function(answerData, htmlMeta) {
     var tmpId = this._getId();
-    return '<div class="' + htmlMeta.answerCssClass + '"><div class="exam-js-answer-container"><div class="exam-js-answer-number"><input id="' + tmpId + '" type="' + answerData.type + '" name="' + answerData.groupID + '" class="exam-js-input" data-answer="' + answerData.answer + '" data-answer-type="' + answerData.type + '"/> ' + answerData.number + ')' + '</div>' + '<div class="exam-js-answer">' + htmlMeta.html + '</div></div></div>';
+    return '<div class="' + htmlMeta.answerCssClass + '"><div class="exam-js-answer-container"><div class="exam-js-answer-number"><input id="' + tmpId + '" type="' + answerData.inputType + '" name="' + answerData.groupID + '" class="exam-js-input" data-answer="' + answerData.answer + '" data-answer-type="' + answerData.type + '"/> ' + answerData.number + ')' + '</div>' + '<div class="exam-js-answer">' + htmlMeta.html + '</div></div></div>';
   };
   ExamjsTranslator.prototype._createImgAnswer = function(answerData) {
     var html = '<div><img src="' + answerData.answer + '" class="exam-js-img"/></div>';
+    answerData.type = 'image';
     return this._createAnswerHtml(answerData,{answerCssClass: 'exam-js-img-question', html: html});
   };
   ExamjsTranslator.prototype._createAudioAnswer = function(answerData) {
     var html = '<div><audio controls src="' +answerData.answer + '" preload="none"/></div>';
+    answerData.type = 'audio';
     return this._createAnswerHtml(answerData,{answerCssClass: 'exam-js-media-question', html: html});
   };
   ExamjsTranslator.prototype._createVideoAnswer = function(answerData) {
     var html = '<div><video controls width="400" height="300" src="' + answerData.answer + '" preload="none" class="exam-js-video-answer"/></div>';
+    answerData.type = 'video';
     if (answerData.answer.indexOf('youtu') >= 0) {
       html = this._createYoutubeHtml(answerData.answer);
     }
-    return this._createAnswerHtml(answerData,{answerCssClass: 'exam-js-media-quesion', html: html});
+    return this._createAnswerHtml(answerData,{answerCssClass: 'exam-js-media-question', html: html});
   };
   ExamjsTranslator.prototype._createTextAnswer = function(answerData) {
+    answerData.type = 'text';
     return this._createAnswerHtml(answerData, {
       answerCssClass: 'exam-js-text-question',
-      html: '<div>' + answerData.answer.answer + '</div>'
+      html: '<div>' + answerData.answer + '</div>'
     });
   };
   ExamjsTranslator.prototype._createTypedQuestion = function(question, type, answerGenerator) {
@@ -1951,7 +1955,7 @@ var ExamjsTranslator = (function() {
     return '<form id="' + question.htmlID + '" class="exam-js-question">' + '<div>' + question.question + '</div><div>' + question.answers.map(function(a) {
       return answerGenerator.call(self, {
         answer: a.answer,
-        type: type,
+        inputType: type,
         groupID: groupID,
         number: question.answers.indexOf(a) + 1
       });
--- test/integration-tests/media-questions-spec.js
+++ test/integration-tests/media-questions-spec.js
@@ -4,8 +4,9 @@ var exam = require('../../dist/exam.js');
 
 describe('Media question tests', function() {
   describe('common media type question', function() {
-    it('should parse VDIEO questions', function() {
+    it('should parse VIDEO questions', function() {
       var result = exam.parse('ТЕСТЫ\r\rВИДЕО\r\rTest question?\r+a1\r+a2\r\rКОНЕЦ ТЕСТОВ');
+      console.log(result);
       assert.equal(result.html, '<form id="exam-js-0" class="exam-js-question"><div>Test question?</div><div><div class="exam-js-media-question"><div class="exam-js-answer-container"><div class="exam-js-answer-number"><input id="exam-js-1" type="checkbox" name="exam-js-group-0" class="exam-js-input" data-answer="a1" data-answer-type="video"/> 1)</div><div class="exam-js-answer"><div><video controls width="400" height="300" src="a1" preload="none" class="exam-js-video-answer"/></div></div></div></div><div class="exam-js-media-question"><div class="exam-js-answer-container"><div class="exam-js-answer-number"><input id="exam-js-2" type="checkbox" name="exam-js-group-0" class="exam-js-input" data-answer="a2" data-answer-type="video"/> 2)</div><div class="exam-js-answer"><div><video controls width="400" height="300" src="a2" preload="none" class="exam-js-video-answer"/></div></div></div></div></div></form>', true);
     });
 
