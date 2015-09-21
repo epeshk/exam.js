ba937d0a448265843c82a5ebd879818e50aaef7b
Sergey Golovin <golovim@gmail.com>
2015-09-21 20:34:32 +0300

    add mock object for QuestionManager

--- test/unit-tests/exam-question-manager-spec.js
+++ test/unit-tests/exam-question-manager-spec.js
@@ -2,12 +2,26 @@
 describe('QuestionManager', function() {
   var questionManager;
   beforeEach(function() {
-    questionManager = new QuestionManager();
+    var parsedSource = {
+      expressions: [{
+        questions: [{
+          answer: 'yes',
+          isRight: true
+        }],
+        html: '<form class="exam-js-question">test?<input id="exam-js-78" type="text" class="exam-js-input"/></form>',
+        htmlID: 'exam-js-1',
+        question: 'test?'
+      }],
+      type: 'tests-setion'
+    };
+    questionManager = new QuestionManager(parsedSource);
   });
 
   describe('getResults()', function() {
     it('should return a result object', function() {
+      var result = questionManager.getResults();
 
+      expect(result).not.toBe(null);
     });
   });
 });
